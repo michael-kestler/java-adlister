dfbf1da76e24c498bc6a1644aa2d63f581b99d3c
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-13 12:13:24 -0700

    completed hello world servlet and counter servlet tasks

--- src/main/java/HelloWorldServletPractice.java
+++ src/main/java/HelloWorldServletPractice.java
@@ -1,13 +1,10 @@
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.http.*;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldServletPractice extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -15,11 +12,5 @@ public class HelloWorldServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello, World!</h1>");
     }
-
-
-
-
-
-
 
 }
