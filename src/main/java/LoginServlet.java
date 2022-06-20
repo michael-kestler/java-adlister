3f90e686c8f264ea75397712d20d0de94384fdc6
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-20 05:40:27 -0700

    moved login logic from jsp to login servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -12,13 +12,15 @@ public class LoginServlet extends HttpServlet {
     }
 
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    if (request.getMethod().equalsIgnoreCase("post")) {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         if (username.equals("admin") && password.equals("password")) {
             response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
         }
-    }
+
 
 
 
